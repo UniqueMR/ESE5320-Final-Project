f8874b1206ece7b10b4c8c168797be5397f4e846
Runlong Hu <1684525025@qq.com>
2023-11-04 12:33:01 -0400

    modify cdc
    specify the behavior for the first and the last chunk
    add p2's pdf

--- Server/cdc.cpp
+++ Server/cdc.cpp
@@ -40,6 +40,11 @@ void cdc(unsigned char* buffer, std::vector<std::string> &chunks, unsigned int b
     //do some operation to get the chunks 
     std::cout << "Using cdc to split the input into chunks ..." << std::endl;
     std::string chunk;
+    //initialize the first chunk
+    for(unsigned int i = 0; i < WIN_SIZE; i++){
+        chunk.push_back(buffer[i]);
+    }
+
 	for(unsigned int i = WIN_SIZE; i < buff_size - WIN_SIZE; i++){
         chunk.push_back(buffer[i]);
         uint64_t hash = hash_func(buffer, i);
@@ -48,6 +53,15 @@ void cdc(unsigned char* buffer, std::vector<std::string> &chunks, unsigned int b
             chunk.clear();
         }
 	}
+
+    //push the remaining characters into the last chunk
+    //push the last chunk into chunks
+    for(unsigned int i = buff_size - WIN_SIZE; i < buff_size; i++){
+        chunk.push_back(buffer[i]);
+    }
+    chunks.push_back(chunk);
+    chunk.clear();
+
     last_hash = 0;
     std::cout << "Finish generating chunks using cdc." << std::endl;
     std::cout << "The number of chunk is " << chunks.size() << std::endl;
