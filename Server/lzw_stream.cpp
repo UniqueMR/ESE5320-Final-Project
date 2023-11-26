1a08502c87ebb18467335c546e75c96541301dac
Runlong Hu <1684525025@qq.com>
2023-11-25 21:33:20 -0500

    add compress length to stream

--- Server/lzw_stream.cpp
+++ Server/lzw_stream.cpp
@@ -6,24 +6,25 @@ static void read_input(unsigned char *s1, hls::stream<unsigned char>& chr_stream
 mem_rd:
     for(int i = 1; i < length; i++){
 // #pragma HLS LOOP_TRIPCOUNT min = MAX max = length
-        chr_stream << s1[i];
+        chr_stream.write(s1[i]);
     }
 }
 
-static void write_result(uint16_t* out_code, hls::stream<uint16_t>& cmprs_stream, int *out_len){
+static void write_result(uint16_t* out_code, hls::stream<uint16_t>& cmprs_stream, hls::stream<int> &cmprs_len_stream, int *out_len){
 mem_wr:
+    *out_len = cmprs_len_stream.read();
     for(int i = 0; i < *out_len; i++){
 // #pragma HLS LOOP_TRIPCOUNT min = *out_len max = *out_len
         out_code[i] = cmprs_stream.read();
     }
 }
 
-static void compute_lzw(hls::stream<unsigned char>& chr_stream, hls::stream<uint16_t>& cmprs_stream, int length, int *out_len, unsigned long *hash_table, assoc_mem* my_assoc_mem){
+static void compute_lzw(hls::stream<unsigned char>& chr_stream, hls::stream<uint16_t>& cmprs_stream, int length, hls::stream<int> &cmprs_len_stream, unsigned long *hash_table, assoc_mem* my_assoc_mem){
 execute:
     int nxt_code = 256, prefix_code = chr_stream.read();
     unsigned int code = 0;
     char nxt_char = 0;
-    *out_len = 0;
+    int local_cmprs_len = 0;
 
     for(int i = 1; i < length; i++){
 // #pragma HLS LOOP_TRIPCOUNT min = length max = length
@@ -34,8 +35,8 @@ execute:
         lookup(hash_table, my_assoc_mem, (prefix_code << 8) + nxt_char, &hit, &code);
         
         if(!hit){
-            cmprs_stream << prefix_code;
-            (*out_len) += 1;
+            cmprs_stream.write(prefix_code);
+            local_cmprs_len += 1;
 
             //detect hash collision
             //add to assoc_mem in the case of detecting hash collision
@@ -47,9 +48,10 @@ execute:
         }
         else{
             prefix_code = code;
-            if(i + 1 == length) cmprs_stream << prefix_code;
+            if(i + 1 == length) cmprs_stream.write(prefix_code);
         }
     }
+    cmprs_len_stream.write(local_cmprs_len);
 }
 
 static void init_mem(unsigned long *hash_table, assoc_mem* my_assoc_mem){
@@ -81,14 +83,16 @@ static void init_mem(unsigned long *hash_table, assoc_mem* my_assoc_mem){
 static void hardware_encoder(unsigned char* s1, int length, uint16_t* out_code, int *out_len, unsigned long* hash_table, assoc_mem* my_assoc_mem){
     hls::stream<unsigned char> chr_stream("char_stream");
     hls::stream<uint16_t> cmprs_stream("compress_stream");
+    hls::stream<int> cmprs_len_stream("compress_length_stream");
 
 #pragma HLS STREAM variable = chr_stream depth = 32
 #pragma HLS STREAM variable = cmprs_stream depth = 32
+#pragma HLS STREAM variable = cmprs_len_stream depth = 32
 
 #pragma HLS dataflow
     read_input(s1, chr_stream, length);
-    compute_lzw(chr_stream, cmprs_stream, length, out_len, hash_table, my_assoc_mem);
-    write_result(out_code, cmprs_stream, out_len);
+    compute_lzw(chr_stream, cmprs_stream, length, cmprs_len_stream, hash_table, my_assoc_mem);
+    write_result(out_code, cmprs_stream, cmprs_len_stream, out_len);
 }
 
 void lzw_stream(unsigned char* s1, int length, uint16_t* out_code, int *out_len){
